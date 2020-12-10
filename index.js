79dc9eccb7685e1564a7889596fe9ca3ba20f94b
Stephen Smit <stephen.smit@goziohealth.com>
2020-12-10 17:25:23 -0500

    added linting added an optional sms notifier. This defaults to false and is intended for more advanced users who want to have sms notifications when the store hits the queue added logger

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
 const open = require('open');
 const promptly = require('promptly');
+const puppeteer = require('puppeteer');
 
-const {checkForPlaystationDirectRedirect} = require("./utils");
+const { checkForPlaystationDirectRedirect, logger, notify } = require('./utils');
 
 /** Constants */
-let numTries = 1;
 const playstationType = {
-    "disc": {
-        "id": 3005816,
-        "url": "https://direct.playstation.com/en-us/consoles/console/playstation5-console.3005816",
-    },
-    "digital": {
-        "id": 3005817,
-        "url": "https://direct.playstation.com/en-us/consoles/console/playstation5-digital-edition-console.3005817",
-    }
+  disc: {
+    id: 3005816,
+    url: 'https://direct.playstation.com/en-us/consoles/console/playstation5-console.3005816',
+  },
+  digital: {
+    id: 3005817,
+    url: 'https://direct.playstation.com/en-us/consoles/console/playstation5-digital-edition-console.3005817',
+  },
 };
 
 /** Let's do this */
-(async function() {
-    const choice = await promptly.choose("Which version would you like? (disc or digital)", ["disc", "digital"]);
-    console.log(`Searching for PlayStation 5 ${choice} edition...`);
-    
-    const onSuccess = () => {
-        console.log("Found it! Opening queue now...");
-        open(playstationType[choice].url);
-    };
+(async () => {
+  const choice = await promptly.choose('Which version would you like? (disc or digital)', ['disc', 'digital']);
+  // const choice = 'disc';
+  logger.info(`Searching for PlayStation 5 ${choice} edition...`);
 
-    checkForPlaystationDirectRedirect(5000, onSuccess);
-})();
+  const onSuccess = () => {
+    logger.info('Found it! Opening queue now...');
+    notify('Go get your PS5!');
+    open(playstationType[choice].url);
+  };
+
+  checkForPlaystationDirectRedirect(5000, onSuccess, playstationType[choice].id, await puppeteer.launch());
+})().catch((error) => {
+  logger.error(error);
+  notify('There was a problem, check it out!');
+  throw error;
+});
